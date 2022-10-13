d8a52e4c40162387ba7e438f42b2147a9b72471d
GGAYIFRANKY <ggayinew1@gmail.com>
2022-10-13 19:08:44 +0300

    cuisine home page and category pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import Home from './pages/Home';
+import Pages from './pages/Pages';
+import Category from './components/Category';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+    <BrowserRouter>
+      <Category />
+      <Pages />
+    </BrowserRouter>
     </div>
   );
 }
